fe972d3533740856834992e04143b4c28f975f20
Arif <a.syarifudin@windowslive.com>
2023-08-23 19:57:29 +0700

    add fetching method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
-
-//  1. basic rendering
-//  2. v-model
-//  3. v-bind
-//  4. v-for
-//  5. v-if else if
-//  6. v-on
-//  7. keyboard event
+// https://jsonplaceholder.typicode.com/guide/
+//  1. Fetch Data GET
+//  2. Fetch Data POST
+//  3. Fetch Data PUT
+//  4. Fetch Data DELETE
 
 <template>
     <h1>Hello World</h1>
 </template>
-
-
 
 <script>
   import axios from 'axios'
@@ -22,9 +17,64 @@
         }
       },
       methods: {
-        getPost(){
-          
+        postData(){
+          var body = {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+          }
+
+          var header = {
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+
+          let res = axios.post('https://jsonplaceholder.typicode.com/posts',
+            body,
+           {
+             headers: {
+              header
+             }
+          })
+          console.log(res)
+
+        },
+        updateData(){
+          var body = {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+          }
+
+          var header = {
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+
+          let res = axios.put('https://jsonplaceholder.typicode.com/posts/1',
+            body,
+           {
+             headers: {
+              header
+             }
+          })
+          console.log(res)
+
+        },
+        deleteData(){
+          var header = {
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+
+          let res = axios.delete('https://jsonplaceholder.typicode.com/posts/1',
+           {
+             headers: {
+              header
+             }
+          })
+          console.log(res)
         }
+      },
+      mounted(){
+
       }
   }
 </script>
